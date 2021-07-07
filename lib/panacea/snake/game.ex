cabc85a9e2f6affe22de1f6b4f263daba99c76e1
Fábio Fusimoto <fabiofusimoto.pires@hotmail.com>
2021-07-07 10:36:21 -0300

    Snake controls and game over animation

--- lib/panacea/snake/game.ex
+++ lib/panacea/snake/game.ex
@@ -5,22 +5,34 @@ defmodule Panacea.Snake.Game do
   # Game related attributes
   @background_color [0, 0, 0]
   @snake_color [0, 255, 0]
-  @timeout 200 # in miliseconds
+  @timeout 150 # in milliseconds
   @width 18
   @height 18
+  @animation_time 750 #in millis
 
   # The 'positions' array assumes the head is the first element
-  @initial_state %{positions: [{2, 0}, {1, 0}, {0, 0}], direction: "RIGHT", stop: false}
+  @initial_state %{
+    positions: [{5, 0}, {4, 0}, {3, 0}, {2, 0}, {1, 0}, {0, 0}],
+    direction: "RIGHT",
+    next_direction: "RIGHT",
+    stop: false
+  }
 
   # :snake agent will hold the game's state
   @agent :snake
+
+  def agent(), do: @agent
+
+  def width(), do: @width
+
+  def height(), do: @height
 
   def start() do
     Agent.start_link(fn -> @initial_state end, name: @agent)
     Worker.execute(
       fn ->
         Leds.light_all(@background_color)
-        Enum.each(@initial_state[:positions], fn {x, y} -> Leds.light_single(@snake_color, x, y) end)
+        Enum.each(@initial_state[:positions], fn {x, y} -> Leds.light_single(@snake_color, x, y)  end)
         Leds.show_leds()
         Task.start(fn -> update() end)
       end
@@ -32,31 +44,71 @@ defmodule Panacea.Snake.Game do
     Worker.execute(&Worker.reset_if_running/0)
   end
 
-  def stopped?(), do: Agent.get(@agent, fn %{stop: stop} -> stop end)
+  def stopped?() do
+    ongoing_process = Process.whereis(@agent)
+    if ongoing_process do
+      Agent.get(@agent, fn %{stop: stop} -> stop end)
+    else
+      true
+    end
+  end
+
+  defp check_for_collision(head, positions_except_head), do: head in positions_except_head
+
+  defp display_game_over_animation(final_positions) do
+    if (!stopped?()) do
+      Worker.execute(
+        fn ->
+          Leds.light_all(@background_color)
+          Process.sleep(@animation_time)
+          Enum.each(final_positions, fn {x, y} -> Leds.light_single(@snake_color, x, y)  end)
+          Leds.show_leds()
+          Process.sleep(@animation_time)
+        end
+      )
+      Process.sleep(2 * @animation_time)
+      display_game_over_animation(final_positions)
+    end
+  end
+
+  defp game_over(final_positions) do
+    display_game_over_animation(final_positions)
+  end
 
   def update() do
     if !stopped?() do
       Process.sleep(@timeout)
 
       if Process.whereis(@agent) do
-        %{:positions => positions, :direction => direction} = Agent.get(@agent, fn state -> state end)
+        %{:positions => positions, :next_direction => next_direction} = Agent.get(@agent, fn state -> state end)
 
-        {changed?, new_positions} = compute_new_positions(positions, direction)
+        new_positions = compute_new_positions(positions, next_direction)
 
-        if changed? do
-          Agent.update(@agent, fn state -> %{state | positions: new_positions} end)
+        collided? = check_for_collision(
+          Enum.at(new_positions, 0),
+          List.delete_at(new_positions, 0)
+        )
+
+        if collided? do
+          game_over(positions)
+        else
+          Agent.update(@agent, fn state -> %{state | positions: new_positions, direction: next_direction} end)
+
           {new_head_x, new_head_y} = Enum.at(new_positions, 0)
           {previous_tail_x, previous_tail_y} = Enum.at(positions, length(positions) - 1)
+
           Worker.execute(
             fn ->
-              Leds.light_single(@snake_color, new_head_x, new_head_y)
-              Leds.light_single(@background_color, previous_tail_x, previous_tail_y)
-              Leds.show_leds()
+              if {previous_tail_x, previous_tail_y} != {new_head_x, new_head_y} do
+                Leds.light_single(@background_color, previous_tail_x, previous_tail_y)
+                Leds.light_single(@snake_color, new_head_x, new_head_y)
+                Leds.show_leds()
+              end
             end
           )
-        end
 
-        update()
+          update()
+        end
       end
     end
   end
@@ -67,42 +119,50 @@ defmodule Panacea.Snake.Game do
 
     case direction do
       "RIGHT" ->
-        if x_head == (@width - 1) do
-          {false, positions}
-        else
-          new_positions = positions
-          |> List.insert_at(0, {x_head + 1, y_head})
-          |> List.delete_at(snake_length)
-          {true, new_positions}
-        end
+        new_x_head = if x_head == (@width - 1) do
+            0
+          else
+            x_head + 1
+          end
+
+        positions
+        |> List.insert_at(0, {new_x_head, y_head})
+        |> List.delete_at(snake_length)
+
       "LEFT" ->
-        if x_head == 0 do
-          {false, positions}
-        else
-          new_positions = positions
-          |> List.insert_at(0, {x_head - 1, y_head})
-          |> List.delete_at(snake_length)
-          {true, new_positions}
-        end
-      "UP" ->
-        if y_head == (@height - 1) do
-          {false, positions}
-        else
-          new_positions = positions
-          |> List.insert_at(0, {x_head, y_head + 1})
-          |> List.delete_at(snake_length)
-          {true, new_positions}
-        end
+        new_x_head = if x_head == 0 do
+            @width - 1
+          else
+            x_head - 1
+          end
+
+        positions
+        |> List.insert_at(0, {new_x_head, y_head})
+        |> List.delete_at(snake_length)
+
       "DOWN" ->
-        if y_head == 0 do
-          {false, positions}
-        else
-          new_positions = positions
-          |> List.insert_at(0, {x_head, y_head - 1})
+        new_y_head = if y_head == (@height - 1) do
+            0
+          else
+            y_head + 1
+          end
+
+        positions
+        |> List.insert_at(0, {x_head, new_y_head})
+        |> List.delete_at(snake_length)
+
+      "UP" ->
+        new_y_head = if y_head == 0 do
+            @height - 1
+          else
+            y_head - 1
+          end
+
+          positions
+          |> List.insert_at(0, {x_head, new_y_head})
           |> List.delete_at(snake_length)
-          {true, new_positions}
-        end
-      _ -> {false, positions}
+
+      _ ->  positions
     end
   end
  end
--- lib/panacea_web/live/snake_live.ex
+++ lib/panacea_web/live/snake_live.ex
@@ -1,6 +1,7 @@
 defmodule PanaceaWeb.SnakeLive do
   use PanaceaWeb, :live_view
   alias Panacea.Snake.Game, as: Game
+  alias Panacea.Snake.Controls, as: Controls
 
   def mount(_params, _assigns, socket) do
     {:ok, assign(socket, started: false)}
@@ -14,5 +15,22 @@ defmodule PanaceaWeb.SnakeLive do
   def handle_event("stop_game", _value, socket) do
     Game.stop()
     {:noreply, assign(socket, started: false)}
+  end
+
+  def handle_event("handle_key", %{"key" => key}, socket) do
+    case key do
+      k when k in ["w", "W"] ->
+        Controls.up()
+      k when k in ["s", "S"] ->
+        Controls.down()
+      k when k in ["a", "A"] ->
+        Controls.left()
+      k when k in ["d", "D"] ->
+        Controls.right()
+      _ ->
+        nil
+    end
+
+    {:noreply, socket}
   end
 end
